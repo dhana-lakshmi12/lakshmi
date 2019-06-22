6fa46788bb87059b4dbdf5ae7de36ecc1306f97b
unknown <vvpreethi578@gmail.com>
2019-06-22 09:59:03 +0530

    first commit

---
+++ lakshmi/src/main/java/com/example/lakshmi/LakshmiApplication.java
@@ -0,0 +1,13 @@
+package com.example.lakshmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LakshmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LakshmiApplication.class, args);
+	}
+
+}
